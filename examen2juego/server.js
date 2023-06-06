b56ec0e045c1148c84df8e81bdd66c7615ea82f4
Melissa Lucero TANTACALLE SANCHEZ <melissa.tantacalle@uab.edu.bo>
2023-06-05 21:50:03 -0400

    examen3

--- examen2juego/server.js
+++ examen2juego/server.js
@@ -7,6 +7,15 @@ const server = http.createServer(app)
 const io = socketio.listen(server)
 
 app.set("port", process.env.PORT || 3000);
+io.on('connection', (socket)=>{
+    /* socket.on('chat',(msg)=>{
+         console.log('Mensaje: '+msg)
+     })*/
+  socket.on('chat', (msg) =>{
+         io.emit('chat', msg)
+     })
+ 
+ })
 
 require("./socket")(io,app)
 
